4751c3231461e7a6aaa576d3d51e088350f8c165
Bremen Braun <bremen.braun@gmail.com>
2018-05-18 22:05:28 -0500

    More babel fixes

--- test/edogawa.spec.js
+++ test/edogawa.spec.js
@@ -1,10 +1,10 @@
 import test from 'ava';
 import edogawa from '../dist'
 
-console.log('Edogawa is', edogawa)
-test('foo', t => {
-	t.pass();
-});
+test('Initializes without error', t => {
+	const conan = edogawa()
+	t.pass()
+})
 
 test('bar', async t => {
 	const bar = Promise.resolve('bar');
