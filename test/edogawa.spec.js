e5c63d5c86b4591dc7cd44f026a08954473b68e1
Bremen Braun <bremen.braun@gmail.com>
2018-05-18 23:17:18 -0500

    Add tests and placeholders

--- test/edogawa.spec.js
+++ test/edogawa.spec.js
@@ -1,13 +1,30 @@
 import test from 'ava';
-import edogawa from '../dist'
+import edogawa, { matchers } from '../dist'
+
+const buildFinder = async () => {
+	const conan = edogawa()
+
+	const base = __dirname
+	return await conan.instrument([
+		`${base}/repo/**/*.spec.js`,
+		`${base}/repo/**/*.test.js`
+	])
+}
 
 test('Initializes without error', t => {
 	const conan = edogawa()
 	t.pass()
 })
 
-test('bar', async t => {
-	const bar = Promise.resolve('bar');
+test('Locates files for instrumenting', async t => {
+	const finder = await buildFinder()
+	const foundTests = await finder.findTests({
+		matcher: matchers.FILE()
+	})
 
-	t.is(await bar, 'bar');
-});
+	t.is(foundTests.length, 3)
+})
+
+test('Locates describe blocks within a test file', async t => {
+	t.fail('Test not implemented')
+})
